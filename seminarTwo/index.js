ad0738833fb3c6b716b1699b11465f75b27826ad
Doroga80 <rusdorog@yandex.by>
2024-01-15 13:08:09 +0300

    new fule

---
+++ seminarTwo/index.js
@@ -0,0 +1,25 @@
+// index.js
+
+function generateRandomName() {
+    // реализация генерации случайного имени
+  }
+  
+  function generateRandomAddress() {
+    // реализация генерации случайного адреса
+  }
+  
+  function generateRandomDate() {
+    // реализация генерации случайной даты
+  }
+  
+  function generateRandomNumber(min, max) {
+    // реализация генерации случайного числа в заданном диапазоне
+  }
+  
+  module.exports = {
+    generateRandomName,
+    generateRandomAddress,
+    generateRandomDate,
+    generateRandomNumber
+  };
+  
